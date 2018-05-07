ff8a65ac89efa4ca62686830e5c9d1f70e0d4bdf
zhangge1998 <1263061442@qq.com>
2018-05-07 15:10:03 +0800

    react环境

---
+++ myApp/index.js
@@ -0,0 +1,12 @@
+/**
+ * Created by ubuntu on 5/7/18.
+ */
+import React from 'react';
+import ReactDom from 'react-dom'
+ReactDom.render(
+    <div>
+        <h1>todos</h1>
+        <h2>what shuld to be done?</h2>
+    </div>,
+    document.getElementById('root')
+);
